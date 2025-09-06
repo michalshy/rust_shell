3176ed7ff1e56e45199242b4b076fa3c727eba07
michin <niesmialymichal@gmail.com>
2025-09-06 12:02:12 +0200

    Implement shell in rust

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use std::{env, io::{stdin, stdout, Write}, path::Path, process::{Child, Stdio}};
+
+// Later https://www.gnu.org/software/bash/manual/html_node/Basic-Shell-Features.html#Basic-Shell-Features
+
+fn main() {
+    loop {
+        print!("> ");
+        let _ = stdout().flush();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut commands = input.trim().split("|").peekable();
+        let mut previous_command = None;
+
+        while let Some(command) = commands.next() {
+
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+            
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+
+                    previous_command = None;
+                },
+                "exit" => return,
+                command => {
+
+                    let stdin = previous_command.map_or(
+                        Stdio::inherit(), 
+                        |output: Child| Stdio::from(output.stdout.unwrap())
+                    );
+
+                    let stdout = if commands.peek().is_some() {
+                        // To command
+                        Stdio::piped()
+                    } else {
+                        // To shell
+                        Stdio::inherit()
+                    };
+
+
+                    let output = std::process::Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+    
+                    match output {
+                        Ok(output) => { previous_command = Some(output); },
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        }
+                    }
+                }
+            }
+        }
+        if let Some(mut final_command) = previous_command {
+            let _ = final_command.wait();
+        }
+    }
+}
